3469fc64c96b806ab2626fd95221f61b853c1ca2
Rahmatilla1 <xikmatillr@gmail.com>
2024-04-03 15:15:13 +0500

    Back

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- 
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => console.log(`The server ${PORT} is running on the port`));
