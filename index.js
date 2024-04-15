5caaed8ed6672b9e784d942437d88f262a4230c6
Rahmatilla1 <154993072+Rahmatilla1@users.noreply.github.com>
2024-04-15 16:34:10 +0500

    Delete index.js
    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = process.env.PORT || 4001;
-
-app.listen(PORT, () => console.log(`The server ${PORT} is running on the port`));
